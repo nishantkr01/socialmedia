e9026218eac91b3b6c0d25c502c1807df49b9af3
Amit <nishantkr12@outlook.com>
2021-11-23 10:38:39 +0530

    bug fixes

--- NODE-REST-API/routes/auth.js
+++ NODE-REST-API/routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const User = require("../models/User");
+//Register
+router.get("/register", async(req, res)=>{
+    // res.send("Hey its auth routes");
+    const user = await new User({
+        username:"john",
+        email:"john@example.com",
+        password:"123456"
+    });
 
-router.get("/",(req, res)=>{
-    res.send("Hey its auth routes");
+    user.save();
+    res.send("ok")
 });
 
 
